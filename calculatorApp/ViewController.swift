b18d5b0d6b8222989041296a67273443add301b2
Clinton Nwadiukor <113151586+clintonnwad@users.noreply.github.com>
2022-09-21 08:11:30 -0400

    first commit

---
+++ calculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculatorApp
+//
+//  Created by Cee Jaiy on 2022-09-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
