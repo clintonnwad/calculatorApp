680292cd272077cf90f76251c18348af9c84fffd
jovi848 <j.mcintyre@senit.com>
2022-09-24 22:23:42 -0400

    connect ui to UIViewController

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -8,13 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var expressLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func onPress(_ sender: UIButton) {
+        print(sender.titleLabel?.text)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         Number.format(number: "10000000000.0221321234234234234")
         // Do any additional setup after loading the view.
     }
+    
+    
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
